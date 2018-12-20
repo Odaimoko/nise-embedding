7165d8b06cae2ecadf61068d604b2a7dbb42d684
oda <Odaimoko@gmail.com>
2018-12-20 23:53:10 +0800

    auto eval unified_box

--- eval_pt_box.py
+++ eval_pt_box.py
@@ -9,6 +9,8 @@ import visdom
 from pathlib import PurePosixPath
 import pprint
 import matplotlib.pyplot as plt
+
+plt.switch_backend('agg')
 # local packages
 import nise_lib._init_paths
 from nise_lib.core import *
@@ -33,12 +35,24 @@ baseline_tfiou = 'unifed_boxes/valid_task_1_DETbox_allBox_tfIoU_nmsThres_0.05_0.
 propDET_tfiou = 'unifed_boxes/valid_task_-1_DETbox_allBox_propAll_propDET_tfIoU_nmsThres_0.05_0.5'
 propGT_tfiou = 'unifed_boxes/valid_task_-1_DETbox_allBox_propAll_propGT_tfIoU_nmsThres_0.05_0.5'
 
-
 eval_dir = nise_cfg.PATH.UNIFIED_JSON_DIR
+eval_dir = nise_cfg.PATH._UNIFIED_JSON_DIR + 'valid_task_-1_DETbox_allBox_propAll_propGT_tfIoU_nmsThres_0.15_0.30'
 rec, prec, ap = voc_eval_for_pt(dataset_path, eval_dir)
-
 torch.save([rec, prec, ap], os.path.join(*[eval_dir, 'ap.pkl']))
-
+debug_print(eval_dir)
 debug_print('AP:', ap)
-propgt_plot = plt.plot(rec.numpy(), prec.numpy())
-plt.show()
+
+# eval_dirs = os.listdir(nise_cfg.PATH._UNIFIED_JSON_DIR)
+# eval_dirs = sorted(eval_dirs)
+# print(eval_dirs)
+# for eval_dir in eval_dirs:
+#     eval_dir = os.path.join(nise_cfg.PATH._UNIFIED_JSON_DIR, eval_dir)
+#     pks = os.listdir(eval_dir)
+#     if len(pks) < 50:
+#         continue
+#     rec, prec, ap = voc_eval_for_pt(dataset_path, eval_dir)
+#     torch.save([rec, prec, ap], os.path.join(*[eval_dir, 'ap.pkl']))
+#     debug_print(eval_dir)
+#     debug_print('AP:', ap)
+#     propgt_plot = plt.plot(rec.numpy(), prec.numpy())
+#     plt.savefig(os.path.join(eval_dir + '_PRcurve.png'))
